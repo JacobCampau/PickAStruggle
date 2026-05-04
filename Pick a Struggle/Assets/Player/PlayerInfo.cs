cb1fa096d8e60c675f6f9a946b5ef335d91f88d8
Jacob Campau <jacobcampau04@gmail.com>
2026-05-04 14:10:58 -0700

    Update PlayerInfo.cs

--- Pick a Struggle/Assets/Player/PlayerInfo.cs
+++ Pick a Struggle/Assets/Player/PlayerInfo.cs
@@ -2,31 +2,32 @@ using UnityEngine;
 
 public class PlayerInfo : MonoBehaviour
 {
+    /*========== Info For All Players ==========*/
     // Base stats
-    float health = 100;
+    float health;
     float curr_health;
     float speed;
-    float stamina_max = 100;
+    float stamina_max;
     float curr_stamina;
     float stamina_drain;
     float jump_force;
-    float damage = 10;
+    float damage;
     float handling_speed;
     float melee_range;
 
     // Emotion stats
-    float emotion_max = 100;
+    float emotion_max;
     float curr_emotion;
     float emotion_buildup;
 
     // Boost stats
-    float boost_health = 0;
-    float boost_speed = 0;
-    float boost_stamina_drain = 0;
-    float boost_damage = 0;
-    float boost_melee_range = 0;
-    float boost_handling_speed = 0;
-    float boost_emotion_buildup = 0;
+    float boost_health;
+    float boost_speed;
+    float boost_stamina_drain;
+    float boost_damage;
+    float boost_melee_range;
+    float boost_handling_speed;
+    float boost_emotion_buildup;
 
     // Totals -> only affected via code
     float total_health;
@@ -38,21 +39,105 @@ public class PlayerInfo : MonoBehaviour
     float total_emotion_buildup;
 
     // Resistance stats
-    float slash_resist = 0;
-    float bludgeon_resist = 0;
+    float slash_resist;
+    float bludgeon_resist;
+
+    // Online Player Information
+    int playerId;
+    string playerName;
+    Color playerColor;
+
+    // Player State Trackers
+    bool isGrounded;
+    bool isStunned;
+    bool isDead;
+
+    /*============= Player Objects =============*/
     
     public:
         /*============== Initial Call ==============*/
-        PlayerInfo(){}
+        PlayerInfo(){
+            // Set Basics
+            health = 100;
+            curr_health = getHealth();
+            speed;
+            stamina_max = 100;
+            curr_stamina = getStaminaMax();
+            stamina_drain;
+            jump_force;
+            damage = 10;
+            handling_speed;
+            melee_range;
+        
+            // Set Emotions
+            emotion_max = 100;
+            curr_emotion = 0;
+            emotion_buildup;
+        
+            // Set Boosts
+            boost_health = 0;
+            boost_speed = 0;
+            boost_stamina_drain = 0;
+            boost_damage = 0;
+            boost_melee_range = 0;
+            boost_handling_speed = 0;
+            boost_emotion_buildup = 0;
+        
+            // Set Totals
+            setHealth();
+            setSpeed();
+            setStaminaDrain();
+            setDamage();
+            setMeleeRange();
+            setHandlingSpeed();
+        
+            setEmotionalBuildUp();
+
+            setSlashResist(0);
+            setBludgeonResist(0);
+
+            // Set Base Info
+            int playerId = 0;
+            string playerName = "NONE";
+            Color playerColor = Color.white;
+
+            // Active state info
+            bool isGrounded = false;
+            bool isStunned = false;
+            bool isDead = false;
+        }
         
         /*================== Gets ==================*/
         // Boosted gets
-        float getHealth(){return total_health;}
-        float getSpeed(){return total_speed;}
-        float getStaminaDrain(){return total_stamina_drain;}
-        float getDamage(){return total_damage;}
-        float getMeleeRange(){return total_melee_range;}
-        float getHandlingSpeed(){return total_handling_speed;}
+        float getHealth(){
+            setHealth();
+            return total_health;
+        }
+        
+        float getSpeed(){
+            setSpeed();
+            return total_speed;
+        }
+        
+        float getStaminaDrain(){
+            setStaminaDrain();
+            return total_stamina_drain;
+        }
+        
+        float getDamage(){
+            setDamage();
+            return total_damage;
+        }
+        
+        float getMeleeRange(){
+            setMeleeRange();
+            return total_melee_range;
+        }
+        
+        float getHandlingSpeed(){
+            setHandlingSpeed();
+            return total_handling_speed;
+        }
         
         // Base gets (Not including the boosted stats)
         float getCurrentHealth(){return curr_health;}
@@ -101,22 +186,42 @@ public class PlayerInfo : MonoBehaviour
             setHealth();
         }
 
-        void boostSpeed(float boost){boost_speed += boost;}
+        void addBoostSpeed(float boost){
+            boost_speed += boost;
+            setSpeed();
+        }
         
-        void boostStaminaDrain(float boost){boost_stamina_drain += boost;}
+        void addBoostStaminaDrain(float boost){
+            boost_stamina_drain += boost;
+            setStaminaDrain();
+        }
         
-        void boostDamage(float boost){boost_damage += boost;}
+        void addBoostDamage(float boost){
+            boost_damage += boost;
+            setDamage();
+        }
         
-        void boostMeleeRange(float boost){boost_melee_range += boost;}
+        void addBoostMeleeRange(float boost){
+            boost_melee_range += boost;
+            setMeleeRange();
+        }
         
-        void boostHandlingSpeed(float boost){boost_handling_speed += boost;}
+        void addBoostHandlingSpeed(float boost){
+            boost_handling_speed += boost;
+            setHandlingSpeed();
+        }
         
-        void boostEmotionBuildup(float boost){boot_emotion_buildup += boost;}
+        void addBoostEmotionBuildup(float boost){
+            boot_emotion_buildup += boost;
+            setEmotionBuildup();
+        }
 
         // Other
         void dealDamage(float attack_dmg){
-            if(curr_health > 0){curr_health -= attack_dmg;}
-            if(curr_health < 0){curr_health = 0;}
+            if(curr_health > 0)
+                curr_health -= attack_dmg;
+            if(curr_health < 0)
+                curr_health = 0;
         }
 
         void applyStaminaDrain(float time_slice){
