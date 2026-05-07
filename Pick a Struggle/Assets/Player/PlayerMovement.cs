cd82b14078bcf82ab20b8a92f8043ffbbb0410cc
Jacob Campau <jacobcampau04@gmail.com>
2026-05-06 23:31:08 -0700

    Fixed Slope Issues
    Fixed the slope issues, next is the aiming camera, crouching would be next but that requires work on animations

--- Pick a Struggle/Assets/Player/PlayerMovement.cs
+++ Pick a Struggle/Assets/Player/PlayerMovement.cs
@@ -1,49 +1,64 @@
 using UnityEngine;
 using PurrNet;
+using UnityEngine.EventSystems;
+using UnityEditor.SceneManagement;
 
 public class PlayerMovement : NetworkIdentity
 {
     public PlayerStats stats;
 
     // Move stats
-    float moveSpeed; 
-    float staminaMax = 100;
-    float staminaDrain = 5;
-    float currStamina;
-    float jumpForce;
+    private float moveSpeed;
+    [SerializeField] private float moveSpeedMult = 1.25f;
+    private float staminaMax = 100;
+    private float staminaDrain = 5;
+    private float currStamina;
+    private float jumpForce;
 
-    float boostSpeed;
-    float boostStaminaDrain;
+    private float boostSpeed;
+    private float boostStaminaDrain;
 
-    float totalSpeed;
-    float totalStaminaDrain;
+    private float totalSpeed;
+    private float totalStaminaDrain;
 
     // Movement stats (same for all characters)
     [Header("Debug")]
     public bool debug;
 
     [Header("Movement")]
-    public float groundDrag;
-    public float jumpCooldown;
-    public float airMult;
-    bool readyToJump;
+    [SerializeField] private float groundDrag;
+    [SerializeField] private float jumpCooldown;
+    [SerializeField] private float airMult;
+    private bool readyToJump;
 
     [Header("Key Binds")]
     public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode sprintKey = KeyCode.LeftShift;
 
     [Header("Ground Check")]
-    public float playerHeight;
-    public LayerMask whatIsGround;
-    bool grounded;
+    [SerializeField] private float playerHeight;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool grounded;
 
-    public Transform orientation;
+    [Header("Slope Handling")]
+    [SerializeField] private float maxSlopeAngle = 45f;
+    private RaycastHit slopeHit;
+    private bool exitingSlope;
 
-    float horizontalInput;
-    float verticalInput;
+    [Header("Other")]
+    [SerializeField] private Transform orientation;
 
-    Vector3 moveDir;
+    private float horizontalInput;
+    private float verticalInput;
 
-    Rigidbody rb;
+    private Vector3 moveDir;
+
+    private Rigidbody rb;
+
+    public enum MovementState { 
+        walking, sprinting, air
+    }
+    public MovementState state;
 
     protected override void OnSpawned() {
         base.OnSpawned();
@@ -68,7 +83,10 @@ public class PlayerMovement : NetworkIdentity
     }
 
     private void Update() {
-        // ground check
+        // Always running, no matter frames
+        StateHandler();
+
+        // Ground check
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
         if(debug)
             if(grounded) {
@@ -77,9 +95,15 @@ public class PlayerMovement : NetworkIdentity
                 Debug.DrawRay(transform.position, Vector3.down * (playerHeight * 0.5f + 0.2f), Color.red);
             }
 
-        MyInput();
+        // Jump
+        if(Input.GetKey(jumpKey) && grounded && readyToJump) {
+            readyToJump = false;
+            Jump();
 
-        // drag
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+
+        // Drag
         if(grounded) {
             rb.linearDamping = groundDrag;
         } else {
@@ -88,43 +112,76 @@ public class PlayerMovement : NetworkIdentity
     }
 
     private void FixedUpdate() {
+        // Movement
         MovePlayer();
-        SpeedControl();
+
+        // Slope Control
     }
 
-    void MyInput() {
+    Vector3 GetMovementDir() {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
-        // jump
-        if(Input.GetKey(jumpKey) && grounded && readyToJump) {
-            readyToJump = false;
-            Jump();
+        return orientation.forward * verticalInput + orientation.right * horizontalInput;
+    }
 
-            Invoke(nameof(ResetJump), jumpCooldown);
+    void StateHandler() {
+        if(grounded && Input.GetKey(sprintKey)) {
+            // Sprinting
+            state = MovementState.sprinting;
+            SetSprintSpeed();
+        } else if(grounded) {
+            // Walking
+            state = MovementState.walking;
+            SetMoveSpeed();
+        } else {
+            // Air
+            state = MovementState.air;
         }
     }
 
     void MovePlayer() {
         // calc move direction
-        moveDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        moveDir = GetMovementDir();
+
+        // On slope
+        if(OnSlope() && !exitingSlope) {
+            rb.AddForce(GetSlopeMoveDirection() * totalSpeed * 20f, ForceMode.Force);
+
+            if(rb.linearVelocity.y > 0)
+                rb.AddForce(Vector3.down * 80f, ForceMode.Force);
+        }
 
         if(grounded) {
             rb.AddForce(moveDir.normalized * totalSpeed * 10f, ForceMode.Force);
         } else {
             rb.AddForce(moveDir.normalized * totalSpeed * 10f * airMult, ForceMode.Force);
         }
+
+        SpeedControl();
+
+        // Cancel gravity on slope
+        rb.useGravity = !OnSlope();
     }
 
     void SpeedControl() {
-        Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
-        if(flatVel.magnitude > totalSpeed) {
-            Vector3 limitedVel = flatVel.normalized * totalSpeed;
-            rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+        if(OnSlope() && !exitingSlope) {
+            // Limiting speed on slopes
+            if(rb.linearVelocity.magnitude > totalSpeed)
+                rb.linearVelocity = rb.linearVelocity.normalized * totalSpeed;
+        } else {
+            // Limiting speed on floor / air
+            Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+            if(flatVel.magnitude > totalSpeed) {
+                Vector3 limitedVel = flatVel.normalized * totalSpeed;
+                rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+            }
         }
     }
 
     void Jump() {
+        exitingSlope = true;
+
         rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
         // Claude added cartoonsh snap
@@ -133,9 +190,23 @@ public class PlayerMovement : NetworkIdentity
 
     void ResetJump() {
         readyToJump = true;
+        exitingSlope = false;
+    }
+
+    bool OnSlope() {
+        if(Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 0.5f + 0.3f)) {
+            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            return angle < maxSlopeAngle && angle != 0;
+        }
+        return false;
+    }
+
+    Vector3 GetSlopeMoveDirection() {
+        return Vector3.ProjectOnPlane(moveDir, slopeHit.normal).normalized;
     }
 
     // Setters used to ensure the stats are accurate to boosts
     void SetMoveSpeed() { totalSpeed = moveSpeed + boostSpeed; }
+    void SetSprintSpeed() { totalSpeed = moveSpeed * moveSpeedMult + boostSpeed; }
     void SetStaminaDrain() { totalStaminaDrain = staminaDrain + boostStaminaDrain; }
 }
