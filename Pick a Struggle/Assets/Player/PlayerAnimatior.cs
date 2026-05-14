0daa32aead286cdb4a0fa96e73f29eea48bab6d4
Jacob Campau <jacobcampau04@gmail.com>
2026-05-14 14:35:05 -0700

    Update PlayerAnimatior.cs

--- Pick a Struggle/Assets/Player/PlayerAnimatior.cs
+++ Pick a Struggle/Assets/Player/PlayerAnimatior.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class PlayerAnimatior : MonoBehaviour
 {
     private Animator anim;
+    private PlayerMovement player;
+    private RagdollLogic ragdoll;
+    
 
     [SerializeField] private GameObject normalEyes;
     [SerializeField] private GameObject deadEyes;
@@ -10,6 +13,14 @@ public class PlayerAnimatior : MonoBehaviour
 
     private void Update() {
         anim = GetComponent<Animator>();
+        player = GetComponent<PlayerMovement>();
+        ragdoll = GetComponent<RagdollLogic>(); 
+    }
+
+    private void Update(){
+        if(ragdoll.ragdollIsActive){
+        
+        }
     }
 
     public void SetStunEyes() {
